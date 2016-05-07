b327409cd93356478fa47c19e65d590c5a685c6b
Sai Teja Ranuva <sranuva@uci.edu>
2016-05-06 23:48:27 -0700

    Update setup.py

--- crdtpy/client/setup.py
+++ crdtpy/client/setup.py
@@ -1,1 +1,11 @@
 from setuptools import setup
+
+setup(name='crdtpy',
+      version='0.1',
+      description='Python package for CRDT datatype database',
+      url='https://github.com/kishore-narendran/crdt-py.git',
+      author='Kishore Narendran, Sai Teja Ranuva',
+      author_email='sranuva@uci.edu',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
