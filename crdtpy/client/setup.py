662753b26fcaaadc5d2c688bb623491bc0dfd983
Sai Teja Ranuva <sranuva@uci.edu>
2016-05-06 23:58:41 -0700

    Update setup.py

--- crdtpy/client/setup.py
+++ crdtpy/client/setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='crdtpy',
       version='0.1',
       description='Python package for CRDT datatype database',
+      long_description=readme(),
       url='https://github.com/kishore-narendran/crdt-py.git',
       author='Kishore Narendran, Sai Teja Ranuva',
       author_email='sranuva@uci.edu',
+      keywords='Distributed Systems CRDT Strong Eventual Consistency Database',
       license='MIT',
       packages=find_packages(),
       zip_safe=False)
