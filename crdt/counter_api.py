c4c8c3f6d505249b57b2bebd684d7f736ab95501
Kishore Narendran <kishore.narendran09@gmail.com>
2016-05-12 18:49:24 -0700

    Redis insertion functionality added

--- crdt/counter_api.py
+++ crdt/counter_api.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify, request
 from generate_key import generate_random_key
+from redis_manager import redis_manager
+from status_codes import status_codes
 
 
 counter_api_blueprint = Blueprint('Counter', __name__)
@@ -15,11 +17,17 @@ def new_g_counter():
         key = generate_random_key()
 
     ##
-    # TODO - Creating, a new counter and inserting into redis
+    #  Checking if the key is already present in Redis
 
     result_dict = dict()
-    result_dict['status'] = {'message': 'success', 'code': 0}
-    result_dict['key'] = key
+    found_key_flag = redis_manager.get(key) is not None
+    if found_key_flag is False:
+        redis_manager.set(key, 0)
+        result_dict['status'] = status_codes['success']
+        result_dict['key'] = key
+        result_dict['counter'] = 0
+    else:
+        result_dict['status'] = status_codes['existing_key']
 
     return jsonify(result_dict)
 
@@ -28,16 +36,24 @@ def new_g_counter():
 def update_g_counter_state():
     key = request.args.get('key')
     state = request.args.get('state')
+
+    found_key_flag = redis_manager.get(key) is not None
     result_dict = dict()
 
+    ##
+    # Checking if a valid key and state has been requested, and checking if
+    # the key is present in the Redis key/value data store. If both these
+    # conditions are met then the merge operation is performed
+
     if key is None or state is None:
-        result_dict['status'] = {'message': 'failure', 'code': 1,
-                                 'notes': 'Key/State of counter was missing or malformed'}
+        result_dict['status'] = status_codes['missing_key_or_state']
+    elif found_key_flag is False:
+        result_dict['status'] = status_codes['key_not_found']
     else:
         ##
         # TODO - Check for counter with given key, and if there is such a counter, perform merge
 
-        result_dict['status'] = {'message': 'success', 'code': 1}
+        result_dict['status'] = status_codes['success']
 
         ##
         # TODO - Return updated counter value
@@ -57,29 +73,43 @@ def new_pn_counter():
         key = generate_random_key()
 
     ##
-    # TODO - Creating, a new counter and inserting into redis
+    #  Checking if the key is already present in Redis
 
     result_dict = dict()
-    result_dict['status'] = {'message': 'success', 'code': 0}
-    result_dict['key'] = key
+    found_key_flag = redis_manager.get(key) is not None
+    if found_key_flag is False:
+        redis_manager.set(key, 0)
+        result_dict['status'] = status_codes['success']
+        result_dict['key'] = key
+        result_dict['counter'] = 0
+    else:
+        result_dict['status'] = status_codes['existing_key']
 
     return jsonify(result_dict)
 
 
 @counter_api_blueprint.route("/pn/update", methods=['GET'])
-def update_counter_state():
+def update_pn_counter_state():
     key = request.args.get('key')
     state = request.args.get('state')
+
+    found_key_flag = redis_manager.get(key) is not None
     result_dict = dict()
 
+    ##
+    # Checking if a valid key and state has been requested, and checking if
+    # the key is present in the Redis key/value data store. If both these
+    # conditions are met then the merge operation is performed
+
     if key is None or state is None:
-        result_dict['status'] = {'message': 'failure', 'code': 1,
-                                 'notes': 'Key/State of counter was missing or malformed'}
+        result_dict['status'] = status_codes['missing_key_or_state']
+    elif found_key_flag is False:
+        result_dict['status'] = status_codes['key_not_found']
     else:
         ##
         # TODO - Check for counter with given key, and if there is such a counter, perform merge
 
-        result_dict['status'] = {'message': 'success', 'code': 1}
+        result_dict['status'] = status_codes['success']
 
         ##
         # TODO - Return updated counter value
--- crdt/set_api.py
+++ crdt/set_api.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify, request
 from generate_key import generate_random_key
+from redis_manager import redis_manager
+from status_codes import status_codes
 
 
 set_api_blueprint = Blueprint('Set', __name__)
@@ -8,7 +10,6 @@ set_api_blueprint = Blueprint('Set', __name__)
 @set_api_blueprint.route("/g/new", methods=['GET'])
 def new_g_set():
     key = request.args.get('key')
-
     ##
     # If the key in request is empty or if there is no key in request
 
@@ -16,42 +17,55 @@ def new_g_set():
         key = generate_random_key()
 
     ##
-    # TODO - Creating, a new set and inserting into redis
+    #  Checking if the key is already present in Redis
 
     result_dict = dict()
-    result_dict['status'] = {'message': 'success', 'code': 0}
-    result_dict['key'] = key
-    result_dict['set'] = []
+    found_key_flag = redis_manager.get(key) is not None
+    if found_key_flag is False:
+        redis_manager.set(key, [])
+        result_dict['status'] = status_codes['success']
+        result_dict['key'] = key
+        result_dict['set'] = []
+    else:
+        result_dict['status'] = status_codes['existing_key']
 
     return jsonify(result_dict)
 
 
 @set_api_blueprint.route("/g/update", methods=['GET'])
-def update_g_counter_state():
+def update_g_set_state():
     key = request.args.get('key')
     state = request.args.get('state')
+
+    found_key_flag = redis_manager.get(key) is not None
     result_dict = dict()
 
+    ##
+    # Checking if a valid key and state has been requested, and checking if
+    # the key is present in the Redis key/value data store. If both these
+    # conditions are met then the merge operation is performed
+
     if key is None or state is None:
-        result_dict['status'] = {'message': 'failure', 'code': 1,
-                                 'notes': 'Key/State of counter was missing or malformed'}
+        result_dict['status'] = status_codes['missing_key_or_state']
+    elif found_key_flag is False:
+        result_dict['status'] = status_codes['key_not_found']
     else:
         ##
-        # TODO - Check for set with given key, and if there is such a set, perform merge
+        # TODO - Check for counter with given key, and if there is such a counter, perform merge
 
-        result_dict['status'] = {'message': 'success', 'code': 1}
+        result_dict['status'] = status_codes['success']
 
         ##
-        # TODO - Return updated set
-        result_dict['state'] = []
+        # TODO - Return updated counter value
+        result_dict['set'] = []
 
     return jsonify(result_dict)
 
 
 @set_api_blueprint.route("/2p/new", methods=['GET'])
-def new_pn_counter():
-    key = request.args.get('key')
+def new_pn_set():
 
+    key = request.args.get('key')
     ##
     # If the key in request is empty or if there is no key in request
 
@@ -59,33 +73,46 @@ def new_pn_counter():
         key = generate_random_key()
 
     ##
-    # TODO - Creating, a new set and inserting into redis
+    #  Checking if the key is already present in Redis
 
     result_dict = dict()
-    result_dict['status'] = {'message': 'success', 'code': 0}
-    result_dict['key'] = key
-    result_dict['set'] = []
+    found_key_flag = redis_manager.get(key) is not None
+    if found_key_flag is False:
+        redis_manager.set(key, [])
+        result_dict['status'] = status_codes['success']
+        result_dict['key'] = key
+        result_dict['set'] = []
+    else:
+        result_dict['status'] = status_codes['existing_key']
 
     return jsonify(result_dict)
 
 
 @set_api_blueprint.route("/2p/update", methods=['GET'])
-def update_counter_state():
+def update_2p_set_state():
     key = request.args.get('key')
     state = request.args.get('state')
+
+    found_key_flag = redis_manager.get(key) is not None
     result_dict = dict()
 
+    ##
+    # Checking if a valid key and state has been requested, and checking if
+    # the key is present in the Redis key/value data store. If both these
+    # conditions are met then the merge operation is performed
+
     if key is None or state is None:
-        result_dict['status'] = {'message': 'failure', 'code': 1,
-                                 'notes': 'Key/State of counter was missing or malformed'}
+        result_dict['status'] = status_codes['missing_key_or_state']
+    elif found_key_flag is False:
+        result_dict['status'] = status_codes['key_not_found']
     else:
         ##
-        # TODO - Check for set with given key, and if there is such a set, perform merge
+        # TODO - Check for counter with given key, and if there is such a counter, perform merge
 
-        result_dict['status'] = {'message': 'success', 'code': 1}
+        result_dict['status'] = status_codes['success']
 
         ##
-        # TODO - Return updated set value
+        # TODO - Return updated counter value
         result_dict['set'] = []
 
     return jsonify(result_dict)
