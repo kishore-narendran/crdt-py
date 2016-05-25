fa1f055a4b2461a4ed2d24310077a439743d1d16
Kishore Narendran <kishore.narendran09@gmail.com>
2016-05-24 18:20:42 -0700

    Some minor refactoring, and comments

--- crdt/model/gcounter.py
+++ crdt/model/gcounter.py
@@ -1,55 +1,67 @@
 import hot_redis
-from constants import CLIENTS, DATA_TYPES, G_COUNTER
-from ..redis_manager import connection
+
+from crdt.constants import CLIENTS, DATA_TYPES, G_COUNTER
+from crdt.key_utilities import get_client_list_key, get_client_key
+from crdt.redis_manager import connection
 
 
 class GCounter:
     def __init__(self, key):
+        # Setting the key of the GCounter Instance
         self.key = key
-        self.client_list = hot_redis.Set(key=key+'_'+CLIENTS, client=connection)
-        hot_redis.Dict(key=DATA_TYPES)[key] = G_COUNTER
+
+        # Getting/Setting the client list and type of the GCounter instance
+        self.client_list = hot_redis.Set(key=get_client_list_key(key), client=connection)
+        hot_redis.Dict(key=DATA_TYPES, client=connection)[key] = G_COUNTER
 
     def add_client(self, client_id):
-        new_client = self.key+'_'+client_id
+        # Generating a client list key for this key
+        new_client = get_client_key(self.key, client_id)
         with hot_redis.transaction():
+            # Adding a new state to all the existing clients
             for client in self.client_list:
                 hot_redis.Dict(key=client, client=connection)[new_client] = 0
+
+            # Adding client to GCounter instance's client list
             self.client_list.add(new_client)
-            new_client_state = hot_redis.Dict(key=new_client)
+
+            # Adding a new state dictionary for this GCounter client
+            new_client_state = hot_redis.Dict(key=new_client, client=connection)
             for client in self.client_list:
                 new_client_state[client] = 0
 
     def get(self, client_id):
-        cur_client = self.key + '_' + client_id
+        # Getting the client's key for this GCounter
+        current_client_key = get_client_key(self.key, client_id)
+
+        # Merging state from every other client for this GCounter
         for client in self.client_list:
-            self.merge(cur_client, client)
+            self.merge(current_client_key, client)
 
-        cur_client_state = hot_redis.Dict(key=cur_client, client=connection)
+        # Updating the states from all other clients as the client's latest state
+        current_client_state = hot_redis.Dict(key=current_client_key, client=connection)
 
+        # Getting the final merged value
         count = 0
-        for val in cur_client_state.values():
+        for val in current_client_state.values():
             count += int(val)
 
         return count
 
     def merge(self, client_a, client_b):
-        """
-        merge client_a with client_b, state of client_a changes
-        """
+        # Getting Client A and Client B's state
         client_a_state = hot_redis.Dict(key=client_a, client=connection)
         client_b_state = hot_redis.Dict(key=client_b, client=connection)
+
+        # Merging Client A's state with Client B's state and storing in Client A's State
         for client in self.client_list:
             client_a_state[client] = max(int(client_a_state[client]), int(client_b_state[client]))
 
     def set(self, client_id, val):
-        cur_client = self.key + '_' + client_id
-        hot_redis.Dict(key=cur_client, client=connection)[cur_client] = val
 
+        # Generating the current client's key for GCounter
+        current_client_key = get_client_key(self.key, client_id)
 
-
-
-
-
-
-
+        # Updating the client's state with the value to be set
+        hot_redis.Dict(key=current_client_key, client=connection)[current_client_key] = val
 
