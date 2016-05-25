f838738b633cfec015ff34b29f929097fc778670
Kishore Narendran <kishore.narendran09@gmail.com>
2016-05-24 18:21:18 -0700

    Some minor changes to redis_manager

--- crdt/redis_manager.py
+++ crdt/redis_manager.py
@@ -5,12 +5,14 @@ import hot_redis
 class RedisManager:
 
     redis = None
+    connection = None
 
     def __init__(self):
         # url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
         # self.redis = redis.Redis(host=url.hostname, port=url.port, password=url.password)
         self.redis = redis.Redis(host='127.0.0.1', port=6379, db=0)
+        self.connection = hot_redis.HotClient(host='localhost', port=6379)
 
 
 redis_manager = RedisManager().redis
-connection = hot_redis.HotClient(host='localhost', port=6379)
+connection = RedisManager().connection
